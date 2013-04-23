9943bd654948ba84fd301857ed5621f83535a186
Joe Thornber <ejt@redhat.com>
2013-04-23 11:17:50 +0100

    cucumber tests were generating invalid xml metadata.

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -16,7 +16,7 @@ module ThinpWorld
   def write_valid_xml(path)
     File.open(path, "w+") do |f|
       f.write <<EOF
-<superblock uuid="" time="0" transaction="0" data_block_size="128" nr_data_blocks="0">
+<superblock uuid="" time="0" transaction="0" data_block_size="128" nr_data_blocks="1000">
   <device dev_id="0" mapped_blocks="2" transaction="0" creation_time="0" snap_time="0">
     <range_mapping origin_begin="0" data_begin="0" length="2" time="0"/>
   </device>
