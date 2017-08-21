80f9d082d5aa108b5ce3538e2d545e92f15d0b87
Joe Thornber <ejt@redhat.com>
2017-08-21 09:59:45 +0100

    [functional-tests/utils] inc!, dec!

---
+++ functional-tests/utils.scm
@@ -0,0 +1,16 @@
+(library
+  (utils)
+  (export inc!
+          dec!)
+  (import (rnrs))
+
+  (define-syntax inc!
+    (syntax-rules ()
+      ((_ v) (set! v (+ 1 v)))
+      ((_ v n) (set! v (+ n v)))))
+
+  (define-syntax dec!
+    (syntax-rules ()
+      ((_ v) (set! v (- v 1)))
+      ((_ v n) (set! v (- v n)))))
+  )
