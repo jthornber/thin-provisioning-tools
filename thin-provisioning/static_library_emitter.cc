edf61aeec1242a32f2a4516242c0e9c81600d097
Thanos Makatos <thanos.makatos@onapp.com>
2018-08-06 13:26:32 +0100

    add emitter that emitts mappings in binary format

--- thin-provisioning/static_library_emitter.cc
+++ thin-provisioning/static_library_emitter.cc
@@ -1,6 +1,7 @@
 #include "thin-provisioning/shared_library_emitter.h"
 #include <stdexcept>
 #include "contrib/tmakatos_emitter.h"
+#include "contrib/tmakatos_mappings_emitter.h"
 #include <iostream>
 
 using namespace std;
@@ -11,10 +12,11 @@ using namespace thin_provisioning;
 emitter::ptr
 thin_provisioning::create_custom_emitter(string const &shared_lib, ostream &out)
 {
-	if (shared_lib != "tmakatos_emitter.so")
-		throw runtime_error(shared_lib + ": no such emitter");
-
-	return emitter::ptr(new tmakatos_emitter::binary_emitter(out));
+	if (shared_lib == "tmakatos_emitter.so")
+		return emitter::ptr(new tmakatos_emitter::binary_emitter(out));
+	if (shared_lib == "tmakatos_mappings_emitter.so")
+		return emitter::ptr(new tmakatos_mappings_emitter::binary_emitter(out));
+	throw runtime_error(shared_lib + ": no such emitter");
 }
 
 //----------------------------------------------------------------
