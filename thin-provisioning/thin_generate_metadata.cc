a842b4ecfd7370eabe2e16560084f4fc0a9f4528
Ming-Hung Tsai <mtsai@redhat.com>
2020-06-01 00:50:57 +0800

    [thin_generate_metadata] Make use of thin_pool.cc

--- thin-provisioning/thin_generate_metadata.cc
+++ thin-provisioning/thin_generate_metadata.cc
@@ -19,7 +19,7 @@
 #include "base/output_file_requirements.h"
 #include "persistent-data/file_utils.h"
 #include "thin-provisioning/commands.h"
-#include "thin-provisioning/metadata.h"
+#include "thin-provisioning/thin_pool.h"
 #include "version.h"
 
 #include <boost/optional.hpp>
@@ -27,8 +27,6 @@
 #include <unistd.h>
 
 using namespace boost;
-using namespace persistent_data;
-using namespace std;
 using namespace thin_provisioning;
 
 //----------------------------------------------------------------
@@ -40,6 +38,7 @@ namespace {
 			METADATA_OP_FORMAT,
 			METADATA_OP_OPEN,
 			METADATA_OP_CREATE_THIN,
+			METADATA_OP_CREATE_SNAP,
 			METADATA_OP_LAST
 		};
 
@@ -55,7 +54,8 @@ namespace {
 		metadata_operations op;
 		sector_t data_block_size;
 		block_address nr_data_blocks;
-		optional<uint64_t> dev_id;
+		optional<thin_dev_t> dev_id;
+		optional<thin_dev_t> origin;
 		optional<string> output;
 	};
 
@@ -77,87 +77,40 @@ namespace {
 			return false;
 		}
 
+		if (op == METADATA_OP_CREATE_SNAP && (!dev_id || !origin)) {
+			cerr << "no device id provided." << endl;
+			return false;
+		}
+
 		return true;
 	}
 
 	//--------------------------------
 
-	single_mapping_tree::ptr new_mapping_tree(metadata::ptr md) {
-		return single_mapping_tree::ptr(
-			new single_mapping_tree(*md->tm_,
-						mapping_tree_detail::block_time_ref_counter(md->data_sm_)));
-	}
-
-	bool is_device_exists(metadata::ptr md, uint64_t dev_id) {
-		uint64_t key[1] = {dev_id};
-
-		device_tree::maybe_value v1 = md->details_->lookup(key);
-		if (v1)
-			return true;
-
-		dev_tree::maybe_value v2 = md->mappings_top_level_->lookup(key);
-		if (v2)
-			return true;
-
-		return false;
-	}
-
-	//--------------------------------
-
-	metadata::ptr format_metadata(block_manager::ptr bm,
-				      sector_t data_block_size,
-				      block_address nr_data_blocks) {
-		metadata::ptr md(new metadata(bm,
-					      metadata::CREATE,
-					      data_block_size,
-					      nr_data_blocks));
-		md->commit();
-		return md;
-	}
-
-	metadata::ptr open_metadata(block_manager::ptr bm) {
-		metadata::ptr md(new metadata(bm, true));
-		return md;
-	}
-
-	void create_thin(metadata::ptr md, uint64_t dev_id) {
-		uint64_t key[1] = {dev_id};
-
-		if (is_device_exists(md, dev_id))
-			throw runtime_error("device already exists");
-
-		device_tree_detail::device_details details;
-		details.transaction_id_ = md->sb_.trans_id_;
-		details.creation_time_ = md->sb_.time_;
-		details.snapshotted_time_ = details.creation_time_;
-		md->details_->insert(key, details);
-
-		single_mapping_tree::ptr subtree = new_mapping_tree(md);
-		md->mappings_top_level_->insert(key, subtree->get_root());
-		md->mappings_->set_root(md->mappings_top_level_->get_root()); // FIXME: ugly
-
-		md->commit();
-	}
-
-	metadata::ptr open_or_format_metadata(block_manager::ptr bm, flags const &fs) {
+	thin_pool::ptr open_or_create_pool(flags const &fs) {
+		block_manager::ptr bm = open_bm(*fs.output, block_manager::READ_WRITE);
 
 		if (fs.op == flags::METADATA_OP_FORMAT)
-			return format_metadata(bm, fs.data_block_size, fs.nr_data_blocks);
+			return thin_pool::ptr(new thin_pool(bm, fs.data_block_size, fs.nr_data_blocks));
 		else
-			return open_metadata(bm);
+			return thin_pool::ptr(new thin_pool(bm));
 	}
 
 	int generate_metadata(flags const &fs) {
-		block_manager::ptr bm = open_bm(*fs.output, block_manager::READ_WRITE);
-		metadata::ptr md = open_or_format_metadata(bm, fs);
+		thin_pool::ptr pool = open_or_create_pool(fs);
 
 		switch (fs.op) {
 		case flags::METADATA_OP_CREATE_THIN:
-			create_thin(md, *fs.dev_id);
+			pool->create_thin(*fs.dev_id);
+			break;
+		case flags::METADATA_OP_CREATE_SNAP:
+			pool->create_snap(*fs.dev_id, *fs.origin);
 			break;
 		default:
 			break;
 		}
+
+		pool->commit();
 
 		return 0;
 	}
@@ -176,8 +129,12 @@ thin_generate_metadata_cmd::usage(std::ostream &out) const
 	out << "Usage: " << get_name() << " [options]\n"
 	    << "Options:\n"
 	    << "  {-h|--help}\n"
-	    << "  --data-block-size <block size>\n"
-	    << "  --nr-data-blocks <nr>\n"
+	    << "  {--format}\n"
+	    << "  {--create-thin} <dev-id>\n"
+	    << "  {--create-snap} <dev-id>\n"
+	    << "  {--data-block-size} <block size>\n"
+	    << "  {--nr-data-blocks} <nr>\n"
+	    << "  {--origin} <origin-id>\n"
 	    << "  {-o|--output} <output device or file>\n"
 	    << "  {-V|--version}" << endl;
 }
@@ -193,10 +150,11 @@ thin_generate_metadata_cmd::run(int argc, char **argv)
 		{ "output", required_argument, NULL, 'o' },
 		{ "format", no_argument, NULL, 1 },
 		{ "open", no_argument, NULL, 2 },
-		{ "create-thin", no_argument, NULL, 3 },
+		{ "create-thin", required_argument, NULL, 3 },
+		{ "create-snap", required_argument, NULL, 4 },
 		{ "data-block-size", required_argument, NULL, 101 },
 		{ "nr-data-blocks", required_argument, NULL, 102 },
-		{ "dev-id", required_argument, NULL, 301 },
+		{ "origin", required_argument, NULL, 401 },
 		{ "version", no_argument, NULL, 'V' },
 		{ NULL, no_argument, NULL, 0 }
 	};
@@ -221,6 +179,12 @@ thin_generate_metadata_cmd::run(int argc, char **argv)
 
 		case 3:
 			fs.op = flags::METADATA_OP_CREATE_THIN;
+			fs.dev_id = parse_uint64(optarg, "device id");
+			break;
+
+		case 4:
+			fs.op = flags::METADATA_OP_CREATE_SNAP;
+			fs.dev_id = parse_uint64(optarg, "device id");
 			break;
 
 		case 101:
@@ -231,8 +195,8 @@ thin_generate_metadata_cmd::run(int argc, char **argv)
 			fs.nr_data_blocks = parse_uint64(optarg, "nr data blocks");
 			break;
 
-		case 301:
-			fs.dev_id = parse_uint64(optarg, "dev id");
+		case 401:
+			fs.origin = parse_uint64(optarg, "origin");
 			break;
 
 		case 'V':
