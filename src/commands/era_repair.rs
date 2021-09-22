36767bcda6c59802e089d106a2cc17d941a95a3c
Ming-Hung Tsai <mtsai@redhat.com>
2021-09-22 16:58:04 +0800

    [era_repair (rust)] First code drop

---
+++ src/commands/era_repair.rs
@@ -0,0 +1,73 @@
+extern crate clap;
+
+use atty::Stream;
+use clap::{App, Arg};
+use std::path::Path;
+use std::process;
+use std::sync::Arc;
+
+use crate::commands::utils::*;
+use crate::era::repair::{repair, EraRepairOptions};
+use crate::report::*;
+
+pub fn run(args: &[std::ffi::OsString]) {
+    let parser = App::new("era_repair")
+        .version(crate::version::tools_version())
+        .about("Repair binary era metadata, and write it to a different device or file")
+        // flags
+        .arg(
+            Arg::with_name("ASYNC_IO")
+                .help("Force use of io_uring for synchronous io")
+                .long("async-io")
+                .hidden(true),
+        )
+        .arg(
+            Arg::with_name("QUIET")
+                .help("Suppress output messages, return only exit code.")
+                .short("q")
+                .long("quiet"),
+        )
+        // options
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Specify the input device")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Specify the output device")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .required(true),
+        );
+
+    let matches = parser.get_matches_from(args);
+    let input_file = Path::new(matches.value_of("INPUT").unwrap());
+    let output_file = Path::new(matches.value_of("OUTPUT").unwrap());
+
+    let report = if matches.is_present("QUIET") {
+        std::sync::Arc::new(mk_quiet_report())
+    } else if atty::is(Stream::Stdout) {
+        std::sync::Arc::new(mk_progress_bar_report())
+    } else {
+        Arc::new(mk_simple_report())
+    };
+
+    check_input_file(input_file, &report);
+
+    let opts = EraRepairOptions {
+        input: input_file,
+        output: output_file,
+        async_io: matches.is_present("ASYNC_IO"),
+        report: report.clone(),
+    };
+
+    if let Err(reason) = repair(opts) {
+        report.fatal(&format!("{}", reason));
+        process::exit(1);
+    }
+}
--- src/era/mod.rs
+++ src/era/mod.rs
@@ -1,6 +1,7 @@
 pub mod check;
 pub mod dump;
 pub mod ir;
+pub mod repair;
 pub mod restore;
 pub mod superblock;
 pub mod writeset;
