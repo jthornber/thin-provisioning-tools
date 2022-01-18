b531fd78758fead3e339e27ef2ae7ed2c09cc38c
Ming-Hung Tsai <mtsai@redhat.com>
2022-01-18 16:50:51 +0800

    [cache_restore (rust)] Fix the missing LSB in bitset entries
    - Fix the missing LSB in a newly inserted u64 entry
    - Randomize dirty bits in the test xml to validate bitset restoration
    - Add blank hint entries to the test xml for comparison
    - Fix the number of resident blocks in the test xml

--- tests/common/cache_xml_generator.rs
+++ tests/common/cache_xml_generator.rs
@@ -52,15 +52,20 @@ impl CacheGen {
 
 impl XmlGen for CacheGen {
     fn generate_xml(&mut self, v: &mut dyn MetadataVisitor) -> Result<()> {
+        const HINT_WIDTH: usize = 4;
+
         v.superblock_b(&ir::Superblock {
             uuid: "".to_string(),
             block_size: self.block_size,
             nr_cache_blocks: self.nr_cache_blocks,
             policy: "smq".to_string(),
-            hint_width: 4,
+            hint_width: HINT_WIDTH as u32,
         })?;
 
-        let nr_resident = (self.nr_cache_blocks * self.percent_resident as u32) / 100u32;
+        let nr_resident = std::cmp::min(
+            (self.nr_cache_blocks as u64 * self.percent_resident as u64) / 100,
+            self.nr_origin_blocks,
+        ) as u32;
         let mut cblocks = (0..self.nr_cache_blocks).collect::<Vec<u32>>();
         cblocks.shuffle(&mut rand::thread_rng());
         cblocks.truncate(nr_resident as usize);
@@ -70,22 +75,33 @@ impl XmlGen for CacheGen {
         {
             let mut used = HashSet::new();
             let mut rng = rand::thread_rng();
-            for cblock in cblocks {
+            let mut dirty_rng = rand::thread_rng();
+            for cblock in cblocks.iter() {
                 let mut oblock = 0u64;
                 while used.contains(&oblock) {
                     oblock = rng.gen_range(0..self.nr_origin_blocks);
                 }
 
                 used.insert(oblock);
-                // FIXME: dirty should vary
                 v.mapping(&ir::Map {
-                    cblock,
+                    cblock: *cblock,
                     oblock,
-                    dirty: false,
+                    dirty: dirty_rng.gen_ratio(self.percent_dirty as u32, 100),
                 })?;
             }
         }
         v.mappings_e()?;
+
+        let mut hint = ir::Hint {
+            cblock: 0,
+            data: thinp::cache::hint::Hint::default().hint.to_vec(),
+        };
+        v.hints_b()?;
+        for cblock in cblocks {
+            hint.cblock = cblock;
+            v.hint(&hint)?;
+        }
+        v.hints_e()?;
 
         v.superblock_e()?;
         Ok(())
