c13a2ebc4804189bde1a8976b90f680b2efaba8a
Ming-Hung Tsai <mtsai@redhat.com>
2022-03-22 01:51:34 +0800

    [btree (rust)] Check the number of entries

--- src/pdata/btree.rs
+++ src/pdata/btree.rs
@@ -537,7 +537,6 @@ pub fn unpack_node<V: Unpack>(
         }
 
         if !is_root {
-            /*
             let min = header.max_entries / 3;
             if header.nr_entries < min {
                 return Err(node_err_s(
@@ -548,7 +547,6 @@ pub fn unpack_node<V: Unpack>(
                     ),
                 ));
             }
-            */
         }
     }
 
