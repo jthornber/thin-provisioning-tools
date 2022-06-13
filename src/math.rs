3de781455dce1d4a46eafa9596c112af5e7b44e6
Joe Thornber <ejt@redhat.com>
2022-06-13 12:19:53 +0100

    [math] simplify div_up and remove div_down.
    div_down isn't used, and is just '/' after all.

--- src/math.rs
+++ src/math.rs
@@ -1,21 +1,12 @@
-use std::cmp::PartialEq;
-use std::ops::{Add, Div, Rem};
+use std::ops::{Add, Div, Sub};
 
 //-----------------------------------------
 
-pub trait Integer:
-    Sized + Copy + Add<Output = Self> + Div<Output = Self> + Rem<Output = Self> + PartialEq
+pub fn div_up<T>(v: T, divisor: T) -> T
+where
+    T: Copy + Add<Output = T> + Div<Output = T> + Sub<Output = T> + From<u8>,
 {
-    fn zero() -> Self;
-    fn one() -> Self;
-}
-
-pub fn div_up<T: Integer>(v: T, divisor: T) -> T {
-    if v % divisor != Integer::zero() {
-        v / divisor + Integer::one()
-    } else {
-        v / divisor
-    }
+    (v + (divisor - T::from(1u8))) / divisor
 }
 
 #[test]
@@ -23,32 +14,6 @@ fn test_div_up() {
     assert_eq!(div_up(4usize, 4usize), 1);
     assert_eq!(div_up(5usize, 4usize), 2);
     assert_eq!(div_up(34usize, 8usize), 5);
-}
-
-pub fn div_down<T: Integer>(v: T, divisor: T) -> T {
-    v / divisor
-}
-
-//-----------------------------------------
-
-impl Integer for usize {
-    fn zero() -> Self {
-        0
-    }
-
-    fn one() -> Self {
-        1
-    }
-}
-
-impl Integer for u64 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn one() -> Self {
-        1
-    }
 }
 
 //-----------------------------------------
