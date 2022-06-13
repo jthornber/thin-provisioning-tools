ad81ec69fb4b2a2282be76b2aa1c59a237dbb11d
Joe Thornber <ejt@redhat.com>
2022-06-13 14:21:20 +0100

    [io_engine] Pull SyncIoEngine out to own file

--- src/io_engine/mod.rs
+++ src/io_engine/mod.rs
@@ -1,8 +1,10 @@
-pub mod io_engine;
 pub mod async_;
+pub mod io_engine;
+pub mod sync;
 
-pub use crate::io_engine::io_engine::*;
 pub use crate::io_engine::async_::AsyncIoEngine;
+pub use crate::io_engine::io_engine::*;
+pub use crate::io_engine::sync::SyncIoEngine;
 
 #[cfg(test)]
 pub mod core;
