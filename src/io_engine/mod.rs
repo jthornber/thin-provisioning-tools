26d26ceed93b46c64d0fb8fa06cfbca7990cab9d
Joe Thornber <ejt@redhat.com>
2022-06-13 14:40:08 +0100

    [io_engine] rename io_engine/io_engine.rs -> io_engine/base.rs

--- src/io_engine/mod.rs
+++ src/io_engine/mod.rs
@@ -1,9 +1,9 @@
 pub mod async_;
-pub mod io_engine;
+pub mod base;
 pub mod sync;
 
 pub use crate::io_engine::async_::AsyncIoEngine;
-pub use crate::io_engine::io_engine::*;
+pub use crate::io_engine::base::*;
 pub use crate::io_engine::sync::SyncIoEngine;
 
 #[cfg(test)]
