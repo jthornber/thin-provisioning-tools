802c5a2ff63318148aa23c948802450bf474414c
Joe Thornber <ejt@redhat.com>
2022-06-13 11:19:35 +0100

    Remove all C++ code and autoconf use

--- functional-tests/regex-bench.scm
+++
@@ -1,7 +0,0 @@
-(import (chezscheme)
-        (regex)
-        (loops))
-
-(let ((rx (regex "a*foob+")))
- (time (upto (n 1000000)
-             (rx "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafoobbbbbbb"))))
--- get-gmock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-
-git clone https://github.com/google/googletest --branch release-1.10.0 --depth 1
