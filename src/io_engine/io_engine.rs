03ef659ab8d0f14f440e4861260b32306e07dce9
Joe Thornber <ejt@redhat.com>
2022-06-13 14:21:20 +0100

    [io_engine] Pull SyncIoEngine out to own file

--- src/io_engine/io_engine.rs
+++ src/io_engine/io_engine.rs
@@ -1,12 +1,11 @@
 use safemem::write_bytes;
 use std::alloc::{alloc, dealloc, Layout};
-use std::fs::File;
-use std::fs::OpenOptions;
 use std::io::{self, Result};
 use std::os::unix::fs::{FileExt, OpenOptionsExt};
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::sync::{Condvar, Mutex};
+use std::path::Path;
 
 use crate::file_utils;
 
@@ -82,78 +81,3 @@ pub fn get_nr_blocks(path: &Path) -> io::Result<u64> {
 }
 
 //------------------------------------------
-
-pub struct SyncIoEngine {
-    nr_blocks: u64,
-    file: File,
-}
-
-impl SyncIoEngine {
-    fn open_file(path: &Path, writable: bool, excl: bool) -> Result<File> {
-        let file = OpenOptions::new()
-            .read(true)
-            .write(writable)
-            .custom_flags(if excl {
-                libc::O_EXCL | libc::O_DIRECT
-            } else {
-                libc::O_DIRECT
-            })
-            .open(path)?;
-
-        Ok(file)
-    }
-
-    pub fn new(path: &Path, writable: bool) -> Result<SyncIoEngine> {
-        SyncIoEngine::new_with(path, writable, true)
-    }
-
-    pub fn new_with(path: &Path, writable: bool, excl: bool) -> Result<SyncIoEngine> {
-        let nr_blocks = get_nr_blocks(path)?; // check file mode before opening it
-        let file = SyncIoEngine::open_file(path, writable, excl)?;
-
-        Ok(SyncIoEngine { nr_blocks, file })
-    }
-}
-
-impl IoEngine for SyncIoEngine {
-    fn get_nr_blocks(&self) -> u64 {
-        self.nr_blocks
-    }
-
-    fn get_batch_size(&self) -> usize {
-        1
-    }
-
-    fn read(&self, loc: u64) -> Result<Block> {
-        let b = Block::new(loc);
-        self.file
-            .read_exact_at(b.get_data(), b.loc * BLOCK_SIZE as u64)?;
-        Ok(b)
-    }
-
-    fn read_many(&self, blocks: &[u64]) -> Result<Vec<Result<Block>>> {
-        let mut bs = Vec::new();
-        for b in blocks {
-            bs.push(self.read(*b));
-        }
-        Ok(bs)
-    }
-
-    fn write(&self, b: &Block) -> Result<()> {
-        self.file
-            .write_all_at(b.get_data(), b.loc * BLOCK_SIZE as u64)?;
-        Ok(())
-    }
-
-    fn write_many(&self, blocks: &[Block]) -> Result<Vec<Result<()>>> {
-        let mut bs = Vec::new();
-        for b in blocks {
-            bs.push(self.write(b));
-        }
-        Ok(bs)
-    }
-}
-
-//------------------------------------------
-
-
