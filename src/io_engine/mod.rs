919de17895943d1e356fff397537d108a249a5a7
Joe Thornber <ejt@redhat.com>
2022-06-24 09:15:43 +0100

    remove old async io engine

--- src/io_engine/mod.rs
+++ src/io_engine/mod.rs
@@ -1,9 +1,9 @@
-pub mod async2;
+pub mod async_;
 pub mod base;
 pub mod spindle;
 pub mod sync;
 
-pub use crate::io_engine::async2::AsyncIoEngine;
+pub use crate::io_engine::async_::AsyncIoEngine;
 pub use crate::io_engine::base::*;
 pub use crate::io_engine::spindle::SpindleIoEngine;
 pub use crate::io_engine::sync::SyncIoEngine;
