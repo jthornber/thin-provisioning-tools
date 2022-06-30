9de1e1662c5df0a1a5f9341f1d1195a18839c46f
Ming-Hung Tsai <mtsai@redhat.com>
2022-06-30 21:55:45 +0800

    [io_engine] Remove the file lock in SyncIoEngine
    Resolve merge conflict with commit 6a00f7ffc

--- src/io_engine/sync.rs
+++ src/io_engine/sync.rs
@@ -1,10 +1,8 @@
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Result;
-use std::ops::{Deref, DerefMut};
 use std::os::unix::fs::{FileExt, OpenOptionsExt};
 use std::path::Path;
-use std::sync::{Condvar, Mutex};
 
 use crate::io_engine::*;
 
@@ -12,47 +10,7 @@ use crate::io_engine::*;
 
 pub struct SyncIoEngine {
     nr_blocks: u64,
-    files: Mutex<Vec<File>>,
-    cvar: Condvar,
-}
-
-struct FileGuard<'a> {
-    engine: &'a SyncIoEngine,
-    file: Option<File>,
-}
-
-impl<'a> FileGuard<'a> {
-    fn new(engine: &'a SyncIoEngine, file: File) -> Self {
-        FileGuard {
-            engine,
-            file: Some(file),
-        }
-    }
-}
-
-impl<'a> Deref for FileGuard<'a> {
-    type Target = File;
-
-    fn deref(&self) -> &File {
-        self.file.as_ref().expect("empty file guard")
-    }
-}
-
-impl<'a> DerefMut for FileGuard<'a> {
-    fn deref_mut(&mut self) -> &mut File {
-        match &mut self.file {
-            None => {
-                panic!("empty file guard")
-            }
-            Some(f) => f,
-        }
-    }
-}
-
-impl<'a> Drop for FileGuard<'a> {
-    fn drop(&mut self) {
-        self.engine.put(self.file.take().expect("empty file guard"));
-    }
+    file: File,
 }
 
 impl SyncIoEngine {
@@ -75,47 +33,10 @@ impl SyncIoEngine {
     }
 
     pub fn new_with<P: AsRef<Path>>(path: P, writable: bool, excl: bool) -> Result<Self> {
-        let nr_files = 8;
         let nr_blocks = get_nr_blocks(path.as_ref())?; // check file mode before opening it
-        let mut files = Vec::with_capacity(nr_files);
         let file = SyncIoEngine::open_file(path.as_ref(), writable, excl)?;
-        for _n in 0..nr_files - 1 {
-            files.push(file.try_clone()?);
-        }
-        files.push(file);
 
-        Ok(SyncIoEngine {
-            nr_blocks,
-            files: Mutex::new(files),
-            cvar: Condvar::new(),
-        })
-    }
-
-    fn get(&self) -> FileGuard {
-        let mut files = self.files.lock().unwrap();
-
-        while files.len() == 0 {
-            files = self.cvar.wait(files).unwrap();
-        }
-
-        FileGuard::new(self, files.pop().unwrap())
-    }
-
-    fn put(&self, f: File) {
-        let mut files = self.files.lock().unwrap();
-        files.push(f);
-        self.cvar.notify_one();
-    }
-
-    fn read_(input: &mut File, loc: u64) -> Result<Block> {
-        let b = Block::new(loc);
-        input.read_exact_at(b.get_data(), b.loc * BLOCK_SIZE as u64)?;
-        Ok(b)
-    }
-
-    fn write_(output: &mut File, b: &Block) -> Result<()> {
-        output.write_all_at(b.get_data(), b.loc * BLOCK_SIZE as u64)?;
-        Ok(())
+        Ok(SyncIoEngine { nr_blocks, file })
     }
 }
 
@@ -133,27 +54,30 @@ impl IoEngine for SyncIoEngine {
     }
 
     fn read(&self, loc: u64) -> Result<Block> {
-        SyncIoEngine::read_(&mut self.get(), loc)
+        let b = Block::new(loc);
+        self.file
+            .read_exact_at(b.get_data(), b.loc * BLOCK_SIZE as u64)?;
+        Ok(b)
     }
 
     fn read_many(&self, blocks: &[u64]) -> Result<Vec<Result<Block>>> {
-        let mut input = self.get();
         let mut bs = Vec::new();
         for b in blocks {
-            bs.push(SyncIoEngine::read_(&mut input, *b));
+            bs.push(self.read(*b));
         }
         Ok(bs)
     }
 
     fn write(&self, b: &Block) -> Result<()> {
-        SyncIoEngine::write_(&mut self.get(), b)
+        self.file
+            .write_all_at(b.get_data(), b.loc * BLOCK_SIZE as u64)?;
+        Ok(())
     }
 
     fn write_many(&self, blocks: &[Block]) -> Result<Vec<Result<()>>> {
-        let mut output = self.get();
         let mut bs = Vec::new();
         for b in blocks {
-            bs.push(SyncIoEngine::write_(&mut output, b));
+            bs.push(self.write(b));
         }
         Ok(bs)
     }
