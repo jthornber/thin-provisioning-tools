282b04e29524b08ff768d7e77ed621a59d044d97
Joe Thornber <ejt@redhat.com>
2022-07-22 08:59:38 +0100

    [sync_copier] Fix bug where CopyOps were getting reordered.
    By returning (good, bad) vectors from the read/write functions we were
    losing the relationship between the copy and its buffer.
    
    Now we return a bitset indicating success and pass this along with all
    the CopyOps to the read and write functions.

--- src/io_engine/sync_copier.rs
+++ src/io_engine/sync_copier.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Context, Result};
+use roaring::RoaringBitmap;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::os::unix::fs::OpenOptionsExt;
@@ -93,14 +94,17 @@ impl SyncCopier {
         })
     }
 
-    // Returns (success, failed) ops.
+    // Returns a bitset that indicates whether the read succeeded for
+    // that op (true == succeeded).
     fn do_reads(
         src: &Arc<Mutex<File>>,
         offset: u64,
         block_size: usize,
         ops: &[CopyOp],
         buffer: &mut [u8],
-    ) -> (Vec<CopyOp>, Vec<CopyOp>) {
+    ) -> RoaringBitmap {
+        let mut success_bits = RoaringBitmap::new();
+
         // Split the buffer up into block size chunks
         let mut bufs: Vec<Option<&mut [u8]>> =
             buffer.chunks_mut(block_size).map(|buf| Some(buf)).collect();
@@ -122,8 +126,6 @@ impl SyncCopier {
 
         // issue the io
         let src = src.lock().unwrap();
-        let mut good = Vec::with_capacity(ops.len());
-        let mut bad = Vec::new();
         for op in reads {
             // build io vec
             let mut iovec: Vec<&mut [u8]> = Vec::with_capacity(op.indexes.len());
@@ -142,25 +144,17 @@ impl SyncCopier {
                 Ok(results) => {
                     for (i, v) in results.iter().enumerate() {
                         let index = op.indexes[i as usize];
-                        let cop = ops[index].clone();
-                        if v.is_err() {
-                            bad.push(cop);
-                        } else {
-                            good.push(cop);
+                        if v.is_ok() {
+                            success_bits.insert(index as u32);
                         }
                     }
                 }
                 Err(_) => {
-                    // error everything
-                    for i in 0..op.indexes.len() {
-                        let index = op.indexes[i];
-                        let cop = ops[index].clone();
-                        bad.push(cop);
-                    }
+                    // error everything, this is implicit since success_bits is empty.
                 }
             }
         }
-        (good, bad)
+        success_bits
     }
 
     fn do_writes(
@@ -168,18 +162,24 @@ impl SyncCopier {
         offset: u64,
         block_size: usize,
         ops: &[CopyOp],
+        select_bits: &RoaringBitmap,
         buffer: &[u8],
-    ) -> (Vec<CopyOp>, Vec<CopyOp>) {
+    ) -> RoaringBitmap {
+        let mut success_bits = RoaringBitmap::new();
+
         // Split the buffer up into block size chunks
         let mut bufs: Vec<Option<&[u8]>> = buffer.chunks(block_size).map(|buf| Some(buf)).collect();
 
         let mut writes = Vec::with_capacity(ops.len());
         for (buffer_index, op) in ops.iter().enumerate() {
-            writes.push(Op {
-                block_begin: op.dst,
-                block_end: op.dst + 1,
-                indexes: vec![buffer_index],
-            });
+            // Only write blocks where the read succeeded previously
+            if select_bits.contains(buffer_index as u32) {
+                writes.push(Op {
+                    block_begin: op.dst,
+                    block_end: op.dst + 1,
+                    indexes: vec![buffer_index],
+                });
+            }
         }
 
         // sort by block_begin so the IO will be ordered.
@@ -190,8 +190,6 @@ impl SyncCopier {
 
         // issue the io
         let dst = dst.lock().unwrap();
-        let mut good = Vec::with_capacity(ops.len());
-        let mut bad = Vec::new();
         for op in writes {
             // build io vec
             let mut iovec: Vec<&[u8]> = Vec::with_capacity(op.indexes.len());
@@ -210,25 +208,17 @@ impl SyncCopier {
                 Ok(results) => {
                     for (i, v) in results.iter().enumerate() {
                         let index = op.indexes[i as usize];
-                        let cop = ops[index].clone();
-                        if v.is_err() {
-                            bad.push(cop);
-                        } else {
-                            good.push(cop);
+                        if v.is_ok() {
+                            success_bits.insert(index as u32);
                         }
                     }
                 }
                 Err(_) => {
-                    // error everything
-                    for i in 0..op.indexes.len() {
-                        let index = op.indexes[i];
-                        let cop = ops[index].clone();
-                        bad.push(cop);
-                    }
+                    // error everything, this is implicit since success_bits is empty.
                 }
             }
         }
-        (good, bad)
+        success_bits
     }
 }
 
@@ -241,7 +231,7 @@ impl Copier for SyncCopier {
         let stats = Arc::new(RwLock::new(CopyStats::new(ops.len() as u64)));
 
         // kick off the writer thread.
-        let (tx, rx) = mpsc::sync_channel::<(Vec<CopyOp>, Buffer)>(1);
+        let (tx, rx) = mpsc::sync_channel::<(Vec<CopyOp>, RoaringBitmap, Buffer)>(1);
         let write_thread = {
             let stats = stats.clone();
             let dst = self.dst.clone();
@@ -253,19 +243,29 @@ impl Copier for SyncCopier {
                     break;
                 }
 
-                let (ops, buffer) = msg.unwrap();
-                let (_, mut bad) =
-                    SyncCopier::do_writes(&dst, offset, block_size, &ops, buffer.get_data());
+                let (ops, read_success, buffer) = msg.unwrap();
+                let write_success = SyncCopier::do_writes(
+                    &dst,
+                    offset,
+                    block_size,
+                    &ops,
+                    &read_success,
+                    buffer.get_data(),
+                );
 
-                if !bad.is_empty() {
+                if write_success.len() != read_success.len() {
                     let mut stats = stats.write().unwrap();
-                    stats.write_errors.append(&mut bad);
+                    for i in 0..ops.len() {
+                        if read_success.contains(i as u32) && !write_success.contains(i as u32) {
+                            stats.write_errors.push(ops[i].clone());
+                        }
+                    }
                 }
 
                 let stats = stats.read().unwrap();
                 stats
                     .nr_copied
-                    .fetch_add(ops.len() as u64, Ordering::SeqCst);
+                    .fetch_add(write_success.len(), Ordering::SeqCst);
                 progress.update(&*stats);
             })
         };
@@ -276,7 +276,7 @@ impl Copier for SyncCopier {
             let buffer_size = chunk.len() * self.block_size as usize;
             let ops: Vec<CopyOp> = chunk.iter().cloned().collect();
             let buffer = Buffer::new(buffer_size, 4096);
-            let (good, mut bad) = SyncCopier::do_reads(
+            let read_success = SyncCopier::do_reads(
                 &self.src,
                 self.src_offset,
                 self.block_size,
@@ -284,12 +284,17 @@ impl Copier for SyncCopier {
                 buffer.get_data(),
             );
 
-            if !bad.is_empty() {
+            if read_success.len() as u32 != ops.len() as u32 {
+                // We had some errors, iterate through to find which ones.
                 let mut stats = stats.write().unwrap();
-                stats.read_errors.append(&mut bad);
+                for i in 0..ops.len() {
+                    if !read_success.contains(i as u32) {
+                        stats.read_errors.push(ops[i].clone());
+                    }
+                }
             }
 
-            tx.send((good, buffer))
+            tx.send((ops, read_success, buffer))
                 .context("error when sending io ops to writer thread")?;
         }
 
