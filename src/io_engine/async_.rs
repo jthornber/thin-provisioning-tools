91c92ccf80c84d1e583f8c2273761cfc33a47ac1
Joe Thornber <ejt@redhat.com>
2025-02-07 14:57:23 +0000

    [io_engine] Remove suggest_nr_threads() from IoEngine

--- src/io_engine/async_.rs
+++ src/io_engine/async_.rs
@@ -64,10 +64,6 @@ impl IoEngine for AsyncIoEngine {
         QUEUE_DEPTH
     }
 
-    fn suggest_nr_threads(&self) -> usize {
-        std::cmp::min(2, num_cpus::get())
-    }
-
     fn read(&self, b: u64) -> Result<Block> {
         let b = Block::new(b);
 
