420c36d3ca42ef6af396ae4b34ca59fd7862175b
Joe Thornber <ejt@redhat.com>
2025-02-26 13:58:56 +0000

    [io_engine] Rewrite AsyncIoEngine to use tokio IoUring
    Reimplement read(), write(), read_many(), and write_many() in
    AsyncIoEngine. These now use the tokio IoUring library, removing
    the rio crate dependency.
    
    This is derived from the following commits in the development branch:
    
    - 3a142ef9886c [async io] reimplement read_many() and write_many()
    - 03a826c959d7 Make read_blocks() a method in IoEngine.
    = b375008d331b Rewrite AsyncIoEngine in terms of a StreamReader

--- src/io_engine/async_.rs
+++ src/io_engine/async_.rs
@@ -1,24 +1,44 @@
-use rio::*;
-use std::collections::BTreeMap;
+use io_uring::{opcode, types};
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{self, Result};
+use std::os::fd::{AsRawFd, RawFd};
 use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
+use std::vec::Vec;
 
+use crate::io_engine::ring_pool::*;
 use crate::io_engine::*;
 
-//------------------------------------------
+//--------------------------------
 
-// We hang waiting for completions on spindle devices if the QUEUE_DEPTH
-// is larger than this.  This doesn't give me confidence in io_uring.
-const QUEUE_DEPTH: usize = 256;
+/// Number of rings each engine creates.  Should be related to the
+/// number of threads that will be concurrently using the engine.
+const NR_RINGS: usize = 16;
+// FIXME: reduce this if it makes no difference to performance.
+const QUEUE_DEPTH: u32 = 256;
+
+//--------------------------------
+
+// A couple of utility functions to make it easier to construct io errors
+fn io_err<T>(msg: &str) -> io::Result<T> {
+    Err(io::Error::other(msg))
+}
+
+fn to_io_result<T, E>(r: std::result::Result<T, E>, msg: &str) -> io::Result<T> {
+    r.map_err(|_| io::Error::other(msg))
+}
+
+//--------------------------------
 
 pub struct AsyncIoEngine {
     input: File,
     nr_blocks: u64,
-    ring: Rio,
+    rings: RingPool,
 }
+
+unsafe impl Sync for AsyncIoEngine {}
+unsafe impl Send for AsyncIoEngine {}
 
 impl AsyncIoEngine {
     pub fn new_with<P: AsRef<Path>>(path: P, writable: bool, excl: bool) -> Result<Self> {
@@ -34,137 +54,242 @@ impl AsyncIoEngine {
             .custom_flags(flags)
             .open(path)?;
 
-        let cfg = rio::Config {
-            depth: QUEUE_DEPTH,
-            io_poll: false,
-            ..Default::default()
-        };
-        let ring = cfg.start()?;
+        let rings = RingPool::new(NR_RINGS, QUEUE_DEPTH)?;
 
         Ok(Self {
             input,
             nr_blocks,
-            ring,
+            rings,
         })
     }
 
     pub fn new<P: AsRef<Path>>(path: P, writable: bool) -> Result<Self> {
         Self::new_with(path, writable, true)
     }
-}
 
-//------------------------------------------
+    fn get_fd(&self) -> RawFd {
+        self.input.as_raw_fd()
+    }
+
+    fn exec_op(&self, op: &io_uring::squeue::Entry) -> Result<io_uring::cqueue::Entry> {
+        self.rings.with_ring(|ring| {
+            // Submit operation
+            let mut sq = ring.submission();
+            let _e = to_io_result(unsafe { sq.push(op) }, "submission queue is full");
+            drop(sq);
+
+            // Wait for completion
+            ring.submit_and_wait(1)?;
+
+            // Get completion result
+            let mut cq = ring.completion();
+            if let Some(ret) = cq.next() {
+                Ok(ret)
+            } else {
+                io_err("completion queue is empty")
+            }
+        })
+    }
+}
 
 impl IoEngine for AsyncIoEngine {
     fn get_nr_blocks(&self) -> u64 {
         self.nr_blocks
     }
 
+    // FIXME: remove this
     fn get_batch_size(&self) -> usize {
-        QUEUE_DEPTH
+        QUEUE_DEPTH as usize
     }
 
     fn read(&self, b: u64) -> Result<Block> {
-        let b = Block::new(b);
+        let block = Block::new(b);
+        let loc = b * BLOCK_SIZE as u64;
 
-        let loc = b.loc * BLOCK_SIZE as u64;
-        let completion = self.ring.read_at(&self.input, &b, loc);
+        // Prepare read operation
+        let read_op = opcode::Read::new(
+            types::Fd(self.get_fd()),
+            unsafe { block.get_raw_ptr() },
+            BLOCK_SIZE as u32,
+        )
+        .offset(loc)
+        .build();
 
-        let nr_read = completion.wait()?;
-        if nr_read != BLOCK_SIZE {
-            return Err(io::Error::other("short read"));
+        let ret = self.exec_op(&read_op)?;
+
+        if ret.result() < 0 {
+            return Err(io::Error::from_raw_os_error(-ret.result()));
         }
 
-        Ok(b)
+        if ret.result() as usize != BLOCK_SIZE {
+            return io_err("short read");
+        }
+
+        Ok(block)
     }
 
     fn read_many(&self, blocks: &[u64]) -> Result<Vec<Result<Block>>> {
-        // eprintln!("read_many {:?}", blocks);
-        let blocks: Vec<Block> = blocks.iter().map(|b| Block::new(*b)).collect();
-        let mut completions = Vec::with_capacity(blocks.len());
+        self.rings.with_ring(|ring| {
+            let mut results = Vec::with_capacity(blocks.len());
+            for _ in 0..blocks.len() {
+                results.push(io_err("read not completed"));
+            }
 
-        for (i, b) in blocks.iter().enumerate() {
-            let loc = b.loc * BLOCK_SIZE as u64;
-            let completion = self.ring.read_at(&self.input, b, loc);
-            completions.push((i, completion));
-        }
+            let mut inflight = 0;
+            let mut completed = 0;
+            let mut block_data = Vec::with_capacity(blocks.len());
 
-        let mut errs = BTreeMap::new();
-        for (i, completion) in completions {
-            // eprintln!("waiting for completion {}", i);
-            match completion.wait() {
-                Err(e) => {
-                    errs.insert(i, e);
-                }
-                Ok(nr_read) => {
-                    if nr_read != BLOCK_SIZE {
-                        errs.insert(i, io::Error::other("short read"));
+            // Allocate blocks for all reads
+            for &block_idx in blocks {
+                block_data.push(Some(Block::new(block_idx)));
+            }
+
+            // Process all blocks
+            while completed < blocks.len() {
+                // Submit as many operations as possible
+                while completed + inflight < blocks.len() && inflight < QUEUE_DEPTH as usize {
+                    let idx = completed + inflight;
+                    if let Some(block) = &block_data[idx] {
+                        let block_idx = blocks[idx];
+                        let offset = block_idx * BLOCK_SIZE as u64;
+
+                        // Prepare read operation
+                        let read_op = opcode::Read::new(
+                            types::Fd(self.get_fd()),
+                            unsafe { block.get_raw_ptr() },
+                            BLOCK_SIZE as u32,
+                        )
+                        .offset(offset)
+                        .build()
+                        .user_data(idx as u64);
+
+                        // Push to submission queue
+                        if unsafe { ring.submission().push(&read_op) }.is_err() {
+                            // If queue is full, break and process some completions
+                            break;
+                        }
+
+                        inflight += 1;
+                    } else {
+                        // This shouldn't happen, but just in case
+                        completed += 1;
                     }
                 }
-            }
-        }
 
-        let mut results = Vec::with_capacity(blocks.len());
-        for (i, b) in blocks.into_iter().enumerate() {
-            if let Some(e) = errs.get_mut(&i) {
-                let mut err = io::Error::other("stub");
-                std::mem::swap(&mut err, e);
-                results.push(Err(err));
-            } else {
-                results.push(Ok(b));
-            }
-        }
+                // Submit operations and wait for at least one completion
+                ring.submit_and_wait(1)?;
 
-        Ok(results)
+                // Process completions
+                ring.completion().for_each(|cqe| {
+                    let idx = cqe.user_data() as usize;
+
+                    if cqe.result() < 0 {
+                        // Error occurred
+                        results[idx] = Err(io::Error::from_raw_os_error(-cqe.result()));
+                    } else if cqe.result() as usize != BLOCK_SIZE {
+                        // Short read
+                        results[idx] = io_err("short read");
+                    } else {
+                        // Successful read - take the block from block_data
+                        results[idx] = Ok(block_data[idx].take().unwrap());
+                    }
+
+                    inflight -= 1;
+                    completed += 1;
+                });
+            }
+
+            Ok(results)
+        })
     }
 
-    fn write(&self, b: &Block) -> Result<()> {
-        let loc = b.loc * BLOCK_SIZE as u64;
-        let completion = self.ring.write_at(&self.input, &b, loc);
+    fn write(&self, block: &Block) -> Result<()> {
+        let loc = block.loc * BLOCK_SIZE as u64;
 
-        let nr_written = completion.wait()?;
-        if nr_written != BLOCK_SIZE {
-            return Err(io::Error::other("short write"));
+        // Prepare write operation
+        let write_op = opcode::Write::new(
+            types::Fd(self.get_fd()),
+            unsafe { block.get_raw_ptr() },
+            BLOCK_SIZE as u32,
+        )
+        .offset(loc)
+        .build();
+
+        let ret = self.exec_op(&write_op)?;
+
+        if ret.result() < 0 {
+            return Err(io::Error::from_raw_os_error(-ret.result()));
+        }
+
+        if ret.result() as usize != BLOCK_SIZE {
+            return io_err("short write");
         }
 
         Ok(())
     }
 
     fn write_many(&self, blocks: &[Block]) -> Result<Vec<Result<()>>> {
-        let mut completions = Vec::with_capacity(blocks.len());
+        self.rings.with_ring(|ring| {
+            let mut results = Vec::with_capacity(blocks.len());
+            for _ in 0..blocks.len() {
+                results.push(io_err("write not completed"));
+            }
 
-        for (i, b) in blocks.iter().enumerate() {
-            let loc = b.loc * BLOCK_SIZE as u64;
-            let completion = self.ring.read_at(&self.input, b, loc);
-            completions.push((i, completion));
-        }
+            let mut inflight = 0;
+            let mut completed = 0;
 
-        let mut errs = BTreeMap::new();
-        for (i, completion) in completions {
-            match completion.wait() {
-                Err(e) => {
-                    errs.insert(i, e);
-                }
-                Ok(nr_written) => {
-                    if nr_written != BLOCK_SIZE {
-                        errs.insert(i, io::Error::other("short write"));
+            // Process all blocks
+            while completed < blocks.len() {
+                // Submit as many operations as possible
+                while completed + inflight < blocks.len() && inflight < QUEUE_DEPTH as usize {
+                    let idx = completed + inflight;
+                    let block = &blocks[idx];
+                    let offset = block.loc * BLOCK_SIZE as u64;
+
+                    // Prepare write operation
+                    let write_op = opcode::Write::new(
+                        types::Fd(self.get_fd()),
+                        unsafe { block.get_raw_ptr() },
+                        BLOCK_SIZE as u32,
+                    )
+                    .offset(offset)
+                    .build()
+                    .user_data(idx as u64);
+
+                    // Push to submission queue
+                    if unsafe { ring.submission().push(&write_op) }.is_err() {
+                        // If queue is full, break and process some completions
+                        break;
                     }
+
+                    inflight += 1;
                 }
-            }
-        }
 
-        let mut results = Vec::with_capacity(blocks.len());
-        for i in 0..blocks.len() {
-            if let Some(e) = errs.get_mut(&i) {
-                let mut err = io::Error::other("stub");
-                std::mem::swap(&mut err, e);
-                results.push(Err(err));
-            } else {
-                results.push(Ok(()));
-            }
-        }
+                // Submit operations and wait for at least one completion
+                ring.submit_and_wait(1)?;
 
-        Ok(results)
+                // Process completions
+                ring.completion().for_each(|cqe| {
+                    let idx = cqe.user_data() as usize;
+
+                    if cqe.result() < 0 {
+                        // Error occurred
+                        results[idx] = Err(io::Error::from_raw_os_error(-cqe.result()));
+                    } else if cqe.result() as usize != BLOCK_SIZE {
+                        // Short write
+                        results[idx] = io_err("short write");
+                    } else {
+                        // Successful write
+                        results[idx] = Ok(());
+                    }
+
+                    inflight -= 1;
+                    completed += 1;
+                });
+            }
+
+            Ok(results)
+        })
     }
 }
 
